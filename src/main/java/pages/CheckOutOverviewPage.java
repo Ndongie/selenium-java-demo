6be8fcc510fa90e448cfd2e85b5b02dcb37f909c
Awona <ndongieawona@gmail.com>
2025-10-30 16:56:52 +0100

    using selemiun actions to click the finish button

--- src/main/java/pages/CheckOutOverviewPage.java
+++ src/main/java/pages/CheckOutOverviewPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.SeleniumActions;
 
 import java.time.Duration;
 
@@ -43,7 +44,7 @@ public class CheckOutOverviewPage {
     public OrderConfirmationPage clickFinishButton(){
         logger.info("Clicking the finish button");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
-        driver.findElement(finishButon).click();
+        SeleniumActions.scrollToElementAndClick(driver, driver.findElement(finishButon));
         wait.until(ExpectedConditions.presenceOfElementLocated(orderConfirmationHeading));
         logger.info("Finish button found and successfully clicked");
         return new OrderConfirmationPage(driver);
